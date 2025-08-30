bfdf4865edd1cebde518f0c19fc6fa67c27f782c
Aakarshit Srivastava <113760964+ArkS0001@users.noreply.github.com>
2025-08-30 23:12:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import GermanCoachApp from "./index.jsx"; // or "./App.jsx" if renamed
-import "./index.css"; // optional, if you have global styles
+import GermanCoachApp from "./App.jsx"; // or "./App.jsx" if renamed
+//import "./index.css"; // optional, if you have global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
